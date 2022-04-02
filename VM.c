de0adcad6455b333ca35588e50e9bacbcf05ed5d
Ahnaf Shahriar <shahriarahnaf007@gmail.com>
2022-04-02 16:23:27 -0400

    structured code better with different files

---
+++ VM.c
@@ -0,0 +1,128 @@
+#include "VM.h"
+
+void MAP_VM()
+{
+}
+uint16_t swap16(uint16_t bytes)
+{
+    // since we have the bytes
+    // simply shift the bytes according to the center.
+    return bytes >> 8 | bytes << 8;
+}
+void read_image_file(FILE *file)
+{
+    /* the origin tells us where in LeMem to place the image */
+    uint16_t origin; // creating the start of the VM instructions
+    // reading the file from the PC address of "origin"
+    // reading one instruction at a time
+    // and reading 16 bits at a time- > size of origin
+    fread(&origin, sizeof(origin), 1, file);
+    origin = swap16(origin); // swapping to big endian
+
+    /* we know the maximum file size so we only need one fread */
+    uint16_t max_read = UINT16_MAX - origin;
+    uint16_t *ptr_to_read_data = LeMem + origin;
+    size_t read = fread(ptr_to_read_data, sizeof(uint16_t), max_read, file);
+
+    /* swap to little endian */
+    while (read-- > 0)
+    {
+        *ptr_to_read_data = swap16(*ptr_to_read_data);
+        ++ptr_to_read_data;
+    }
+}
+int read_image(const char *image_path)
+{
+    FILE *file = fopen(image_path, "rb");
+    if (!file) // cant read file
+    {
+        return 0;
+    };
+    read_image_file(file);
+    fclose(file);
+    return 1;
+}
+
+uint16_t sign_extend(uint16_t x, int bit_count)
+{
+    if ((x >> (bit_count - 1)) & 1)
+    {
+        x |= (0xFFFF << bit_count);
+    }
+    return x;
+}
+
+// check the left most bit of the number to see if the register address value is 0 or 1
+void update_flag(uint16_t rNum)
+{
+    if (registers[rNum] == 0)
+    {
+        registers[R_COND] = FL_POS;
+    }
+    else if (registers[rNum] >> 15)
+    {
+        registers[R_COND] = FL_NEG;
+    }
+    else
+    {
+        registers[R_COND] = FL_ZRO;
+    }
+}
+// check key needed for memory access
+uint16_t check_key()
+{
+    fd_set readfds;
+    FD_ZERO(&readfds);
+    FD_SET(STDIN_FILENO, &readfds);
+
+    struct timeval timeout;
+    timeout.tv_sec = 0;
+    timeout.tv_usec = 0;
+    return select(1, &readfds, NULL, NULL, &timeout) != 0;
+}
+void mem_write(uint16_t address, uint16_t valoo)
+{
+    LeMem[address] = valoo;
+}
+// only reads from the address of VM memory
+//  reading keyboard memory calls get char
+uint16_t mem_read(uint16_t address)
+{
+    if (address == MR_KBStatusRegister)
+    {
+        if (check_key())
+        {
+            LeMem[MR_KBStatusRegister] = (1 << 15);
+            LeMem[MR_KBDataRegister] = getchar();
+        }
+        else
+        {
+            LeMem[MR_KBStatusRegister] = 0;
+        }
+    }
+    return LeMem[address];
+}
+
+///////////////////////// UNIX SPECIFIC CODES ////////////////////////////////
+
+struct termios original_tio;
+
+void disable_input_buffering()
+{
+    tcgetattr(STDIN_FILENO, &original_tio);
+    struct termios new_tio = original_tio;
+    new_tio.c_lflag &= ~ICANON & ~ECHO;
+    tcsetattr(STDIN_FILENO, TCSANOW, &new_tio);
+}
+
+int restore_input_buffering()
+{
+    tcsetattr(STDIN_FILENO, TCSANOW, &original_tio);
+}
+void handle_interrupt(int signal)
+{
+    restore_input_buffering();
+    printf("\n");
+    exit(-2);
+}
+//////////////////////////////////////////////////////////////////////
---
+++ VM.h
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <signal.h>
+/* unix */
+#include <unistd.h>
+#include <fcntl.h>
+
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/termios.h>
+#include <sys/mman.h>
+
+// declarations
+void mem_write(uint16_t address, uint16_t valoo);
+void update_flag(uint16_t instr);
+// ALLOACATED   LeMem
+uint16_t LeMem[UINT16_MAX];
+
+// ALL THE INFORMATION ABOUT THE TEMPORARY REGISTER THAT IS INSIDE THE "VM" BEING PROCESSED AT THE MOMENT
+enum
+{
+    R_R0 = 0,
+    R_R1,
+    R_R2,
+    R_R3,
+    R_R4,
+    R_R5,
+    R_R6,
+    R_R7,   // previous jump instruction holder
+    R_PC,   /* program counter */
+    R_COND, // last calculation sign
+    R_COUNT
+};
+
+// OP CODES FOR VIRTUAL CPU, BASICALLY INSTRUCTIONS FOR THE CPU
+enum
+{
+    OP_BRANCH = 0,  /* branch */
+    OP_ADD,         /* add  */
+    OP_LD,          /* load */
+    OP_ST,          /* store */
+    OP_JMP_RES,     /* jump register */
+    OP_AND,         /* bitwise and */
+    OP_LDR,         /* load register */
+    OP_ST_RES,      /* store register */
+    OP_RTI,         /* unused */
+    OP_NOT,         /* bitwise not */
+    OP_LD_I,        /* load indirect */
+    OP_ST_I,        /* store indirect */
+    OP_JMP,         /* jump */
+    OP_RES,         /* reserved (unused) */
+    OP_LD_EFF_ADDR, /* load effective address */
+    OP_TRAP         /* execute trap */
+};
+
+// TRAP CODES
+enum
+{
+    TRAP_GETCHAR = 0x20,   /* get character from keyboard, not echoed onto the terminal */
+    TRAP_OUTPUT = 0x21,    /* output a character */
+    TRAP_PUTSTRING = 0x22, /* output a word string */
+    TRAP_INPUT = 0x23,     /* get character from keyboard, echoed onto the terminal */
+    TRAP_PUTSP = 0x24,     /* output a byte string */
+    TRAP_HALT = 0x25       /* halt the program */
+};
+
+//  LeMem MAPPED REGISTERS
+enum
+{
+    MR_KBStatusRegister = 0xFE00, /* keyboard status */
+    MR_KBDataRegister = 0xFE02    /* keyboard data */
+};
+// store the registers in an array
+uint16_t registers[R_COUNT]; // empty array for how each register is doing
+
+// store the conditions flags
+enum
+{
+    FL_POS = 1 << 0, /* P bascially 1*/
+    FL_ZRO = 1 << 1, /* Z bascially 2 */
+    FL_NEG = 1 << 2, /* N basicaly 4 */
+};
+void MAP_VM();
+uint16_t swap16(uint16_t bytes);
+void read_image_file(FILE *file);
+int read_image(const char *image_path);
+
+uint16_t sign_extend(uint16_t x, int bit_count);
+
+// check the left most bit of the number to see if the register address value is 0 or 1
+void update_flag(uint16_t rNum);
+// check key needed for memory access
+uint16_t check_key();
+void mem_write(uint16_t address, uint16_t valoo);
+// only reads from the address of VM memory
+//  reading keyboard memory calls get char
+uint16_t mem_read(uint16_t address);
+
+///////////////////////// UNIX SPECIFIC CODES ////////////////////////////////
+struct termios original_tio;
+void disable_input_buffering();
+int restore_input_buffering();
+void handle_interrupt(int signal);
+//////////////////////////////////////////////////////////////////////
