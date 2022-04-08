9b32282dfb05a9c742e086844900d66a004a5938
Ahnaf Shahriar <shahriarahnaf007@gmail.com>
2022-04-07 21:43:50 -0400

    implementation 2.0

--- VM.c
+++ VM.c
@@ -108,8 +108,8 @@ void MAP_VM(void)
 
 void log_to_file(struct timespec start, struct timespec end, FILE* log_file, uint16_t op){
     clock_gettime(CLOCK_MONOTONIC_RAW, &end);
-    float delta_us = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) ;
-    fprintf(log_file, "op: %x, time elapsed: %f\n", op, delta_us);
+    float delta_ns = (end.tv_nsec - start.tv_nsec) ;
+    fprintf(log_file, "op: %x, time elapsed: %f\n", op, delta_ns);
 }
 #else
 void MAP_VM(void){
