e483cd30eea7bf60145a2e62f523052e390d2a83
Ahnaf Shahriar <shahriarahnaf007@gmail.com>
2022-04-07 22:39:37 -0400

    numerical speed comparision

--- optimized-compare.py
+++ optimized-compare.py
@@ -1,32 +1,44 @@
-import os
-
 Lefile = open("log_speed.txt", 'r')
-count = 0 
+firstcount = []
+for i in range(0,16):
+   firstcount.append(0) 
+
 line_count = 0
 for line in Lefile:
-    leLine = line.split('.')
-    THEline = leLine[0].split()
-    count += int(THEline[-1], 10)//10000 # last thing in line
+    leLine = (line.split('.')[0]).split()
+    speed = leLine[-1]
+    #find which list to add to. 
+    op=int(leLine[1][0],16)
+    #use the list indext to go to op. 
+    firstcount[op] += int(speed, 10)/100 # last thing in line
     line_count += 1
     if line_count > 10000: break
 Lefile.close()
-print(count)
-print("\n")
-count = 0.00 
-line_count = 0
+
+
 # other file
 Lefile = open("slow_log_speed.txt", 'r')
 
-
-other_count = 0.00 
+count = []
+for i in range(0,16):
+   count.append(0) 
 line_count = 0
 for line in Lefile:
-    leLine = line.split('.')
-    THEline = leLine[0].split()
-    other_count += int(THEline[-1], 10)//10000 # last thing in line
+    leLine = (line.split('.')[0]).split()
+    speed = leLine[-1]
+    #find which list to add to. 
+    op=int(leLine[1][0],16)
+    #use the list indext to go to op. 
+    count[op] += int(speed, 10)/100 # last thing in line
     line_count += 1
     if line_count > 10000: break
-
-print(other_count)
-print("\n")
 Lefile.close()
+
+
+#nice output 
+print(f"{'slow :':<25} {'Fast:':<30} {'DIFF':<60}")
+    
+for i in range(0,len(firstcount)):
+    diff = count[i]/firstcount[i] * 100
+    #print(f"{'Location: ' + location:<25} Revision: {revision}")
+    print(f"{str(count[i]):<25} { str(firstcount[i]):<30} {str(diff) + '%':<60}")
